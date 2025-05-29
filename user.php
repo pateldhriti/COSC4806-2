f3514e601b56a39292269fe5a75ccb5f4e9d1019
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-05-29 18:23:57 +0000

    use FetchALL to display all user list

--- user.php
+++ user.php
@@ -8,7 +8,7 @@ public function get_all_users(){
   $db = db_connect();
   $stmt = $db->prepare("SELECT * FROM users;");
   $stmt->execute();
-  $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $rows;
   
 }
