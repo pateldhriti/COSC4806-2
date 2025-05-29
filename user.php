16e2ca5bf4d99823701bfcc2862e20c12b70790a
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-05-29 18:26:52 +0000

    add create_user method to insert new users into the database

--- user.php
+++ user.php
@@ -12,5 +12,13 @@ public function get_all_users(){
   return $rows;
   
 }
-  
+  public function create_user($username, $password) {
+    $db = db_connect();
+    $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password);");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $rows;
+  }
 }
