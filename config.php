f22143262b0041ff57ec33b25a43012fcb1f8646
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-05-29 17:53:51 +0000

    database configuration file for filess.io MariaDB connection

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'iqi08.h.filess.io');
+define('DB_USER', 'COSC4806_troubleson');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_troubleson');
+define('DB_PORT', '61000');
+
+?>
+       
